29e2bd464e7832f7444634fa5d49d63c17d8ea88
tomaszjedrzejowski <tjedrzejowski.dev@gmail.com>
2025-05-12 19:14:20 +0200

    Add validation for adding new game

--- GameStore.Web/Models/GameDetails.cs
+++ GameStore.Web/Models/GameDetails.cs
@@ -1,10 +1,15 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace GameStore.Web.Models;
 
 public class GameDetails
 {
     public int Id { get; set; }
+    [Required]
+    [StringLength(50)]
     public required string Name { get; set; }
-    public string? GenreId { get; set; }
-    public decimal Price { get; set; }
+    [Required] public string? GenreId { get; set; }
+
+    [Range(0.01, 100)] public decimal Price { get; set; }
     public required DateTime ReleaseDate { get; set; }
 }
