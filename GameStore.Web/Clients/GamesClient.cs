e270f5e57fb776eb154faae0cc8f07197261183f
tomaszjedrzejowski <tjedrzejowski.dev@gmail.com>
2025-05-12 21:57:45 +0200

    fix mockup games genres names to match available genres

--- GameStore.Web/Clients/GamesClient.cs
+++ GameStore.Web/Clients/GamesClient.cs
@@ -9,9 +9,9 @@ public class GamesClient : IGamesClient
     private readonly Genre[] genres = new GenresClient().GetGenres();
 
     private readonly List<GameSummary> games = [
-        new(){Id = 1, Name = "Mock Game I", Genre = "Fighting", Price = 49.99M, ReleaseDate = new DateTime(1992, 7, 15)},
-        new(){Id = 1, Name = "Mock Game II", Genre = "RPG", Price = 19.99M, ReleaseDate = new DateTime(2002, 8, 19)},
-        new(){Id = 1, Name = "Mock Game III", Genre = "Simulator", Price = 9.99M, ReleaseDate = new DateTime(2021, 4, 1)}
+        new(){Id = 1, Name = "Mock Game I", Genre = "Action", Price = 49.99M, ReleaseDate = new DateTime(1992, 7, 15)},
+        new(){Id = 2, Name = "Mock Game II", Genre = "Role-Playing (RPG)", Price = 19.99M, ReleaseDate = new DateTime(2002, 8, 19)},
+        new(){Id = 3, Name = "Mock Game III", Genre = "Racing", Price = 9.99M, ReleaseDate = new DateTime(2021, 4, 1)}
    ];
 
     public GameSummary[] GetGames() => games.ToArray();
