83a6e1a2f04b8662a69995bf137e360263d47445
tomaszjedrzejowski <tjedrzejowski.dev@gmail.com>
2025-05-12 14:48:44 +0200

    example of using asp.net components, forms

--- GameStore.Web/Components/Pages/EditGame.cs
+++ GameStore.Web/Components/Pages/EditGame.cs
@@ -1,6 +1,7 @@
 using GameStore.Web.Clients;
 using GameStore.Web.Models;
 using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Forms;
 
 namespace GameStore.Web.Components.Pages;
 
@@ -25,4 +26,34 @@ public partial class EditGame : ComponentBase
     {
         _gamesClient.AddGame(_game);
     }
+
+    /*
+        <div class="mb-3">
+            <label for="name" class="form-label">Name:</label>
+            <InputText id="name" @bind-Value="_game.Name" class="form-control" />
+        </div>
+     */
+    private RenderFragment RenderInputText(string id, string label, string value) =>
+        builder =>
+        {
+            //<div class="mb-3">
+            builder.OpenElement(0, "div");
+            builder.AddAttribute(1, "class", "mb-3");
+
+            //<label for="name" class="form-label">Name:</label>
+            builder.OpenElement(2, "label");
+            builder.AddAttribute(3, "for", id);
+            builder.AddContent(4, label);
+            builder.CloseElement();
+
+            // <InputText id="name" @bind-Value="_game.Name" class="form-control" />
+            builder.OpenComponent<InputText>(5);
+            builder.AddAttribute(6, "id", id);
+            builder.AddAttribute(7, "class", "form-control");
+            builder.AddAttribute(8, "bind-Value", value);
+            builder.CloseComponent();
+
+            // </div>
+            builder.CloseElement();
+        };
 }
