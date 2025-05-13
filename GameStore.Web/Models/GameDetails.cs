2f5cbbcda75f0662d351f7cfcfbe5605577d0523
tomaszjedrzejowski <tjedrzejowski.dev@gmail.com>
2025-05-13 16:09:14 +0200

    implement string converter for game details model

--- GameStore.Web/Models/GameDetails.cs
+++ GameStore.Web/Models/GameDetails.cs
@@ -1,4 +1,6 @@
 using System.ComponentModel.DataAnnotations;
+using System.Text.Json.Serialization;
+using GameStore.Web.Converters;
 
 namespace GameStore.Web.Models;
 
@@ -11,6 +13,7 @@ public class GameDetails
     public required string Title { get; set; }
 
     [Required(ErrorMessage = "The Genre is required.")]
+    [JsonConverter(typeof(StringConverter))]
     public string? GenreId { get; set; }
 
     [Range(0.01, 100)] public decimal Price { get; set; }
